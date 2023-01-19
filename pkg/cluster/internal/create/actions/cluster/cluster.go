a6a32f2b348e99f1816b0df795a8b326787ad692
Francisco Augusto <faugusto@stratio.com>
2023-01-19 01:47:27 +0100

    More features:
    * New aws.tmpl template
    
    * New logic for workers
    
    * Add validator
    
    Add init function

--- pkg/cluster/internal/create/actions/cluster/cluster.go
+++ pkg/cluster/internal/create/actions/cluster/cluster.go
@@ -3,20 +3,22 @@ package cluster
 import (
 	"bytes"
 	"embed"
+	"fmt"
 	"os"
 	"text/template"
 
+	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v3"
 )
 
-//go:embed templates/aws.eks.tmpl
+//go:embed templates/*
 var ctel embed.FS
 
 // DescriptorFile represents the YAML structure in the cluster.yaml file
 type DescriptorFile struct {
 	APIVersion string `yaml:"apiVersion"`
 	Kind       string `yaml:"kind"`
-	ClusterID  string `yaml:"cluster_id"`
+	ClusterID  string `yaml:"cluster_id" validate:"required"`
 
 	Bastion Bastion `yaml:"bastion"`
 
@@ -28,12 +30,12 @@ type DescriptorFile struct {
 		AssumeRole string `yaml:"assume_role"`
 	} `yaml:"credentials"`
 
-	InfraProvider string `yaml:"infra_provider"`
+	InfraProvider string `yaml:"infra_provider" validate:"required,oneof='aws' 'gcp' 'azure'"`
 
-	K8SVersion   string `yaml:"k8s_version"`
-	Region       string `yaml:"region"`
+	K8SVersion   string `yaml:"k8s_version" validate:"required"`
+	Region       string `yaml:"region" validate:"required"`
 	SSHKey       string `yaml:"ssh_key"`
-	FullyPrivate bool   `yaml:"fully_private"`
+	FullyPrivate bool   `yaml:"fully_private" validate:"boolean"`
 
 	// Networks struct {
 	// 	VPCID   string `yaml:"vpc_id"`
@@ -46,9 +48,9 @@ type DescriptorFile struct {
 	// }
 
 	ExternalRegistry struct {
-		AuthRequired bool   `yaml: auth_required`
+		AuthRequired bool   `yaml: auth_required validate:"boolean"`
 		Type         string `yaml: type`
-		URL          string `yaml: url`
+		URL          string `yaml: url validate:"url"`
 	} `yaml:"external_registry"`
 
 	Keos struct {
@@ -59,40 +61,44 @@ type DescriptorFile struct {
 	} `yaml:"keos"`
 
 	ControlPlane struct {
-		Managed         bool   `yaml:"managed"`
+		Managed         bool   `yaml:"managed" validate:"boolean"`
 		Name            string `yaml:"name"`
 		AmiID           string `yaml:"ami_id"`
-		HighlyAvailable bool   `yaml:"highly_available"`
+		HighlyAvailable bool   `yaml:"highly_available" validate:"boolean"`
 		Size            string `yaml:"size"`
 		Image           string `yaml:"image"`
+		AWS             AWS    `yaml:"aws"`
 	} `yaml:"control_plane"`
 
 	WorkerNodes WorkerNodes `yaml:"worker_nodes"`
 }
 
+type AWS struct {
+	AssociateOIDCProvider bool `yaml:"associate_oidc_provider" validate:"boolean"`
+	Logging               struct {
+		ApiServer         bool `yaml:"api_server" validate:"boolean"`
+		Audit             bool `yaml:"audit" validate:"boolean"`
+		Authenticator     bool `yaml:"authenticator" validate:"boolean"`
+		ControllerManager bool `yaml:"controller_manager" validate:"boolean"`
+		Scheduler         bool `yaml:"scheduler" validate:"boolean"`
+	}
+}
+
 type WorkerNodes []struct {
 	Name             string `yaml:"name"`
 	AmiID            string `yaml:"ami_id"`
-	Quantity         int    `yaml:"quantity"`
-	Size             string `yaml:"size"`
+	Quantity         int    `yaml:"quantity" validate:"required,numeric"`
+	Size             string `yaml:"size" validate:"required"`
 	Image            string `yaml:"image"`
-	ZoneDistribution string `default:"balanced" yaml:"zone_distribution"`
+	ZoneDistribution string `yaml:"zone_distribution" validate:"oneof='balanced' 'unbalanced'"`
 	AZ               string `yaml:"az"`
 	SSHKey           string `yaml:"ssh_key"`
-	Spot             bool   `yaml:"spot"`
-	Disks            []struct {
-		DeviceName string `yaml:"device_name"`
-		Name       string `yaml:"name"`
-		Path       string `yaml:"path,omitempty"`
-		Size       int    `yaml:"size"`
-		Type       string `yaml:"type"`
-		Encrypted  bool   `yaml:"encrypted"`
-		Volumes    []struct {
-			Name string `yaml:"name"`
-			Path string `yaml:"path"`
-			Size string `yaml:"size"`
-		} `yaml:"volumes,omitempty"`
-	} `yaml:"disks"`
+	Spot             bool   `yaml:"spot" validate:"boolean"`
+	RootVolume       struct {
+		Size      int    `yaml:"size" validate:"numeric"`
+		Type      string `yaml:"type"`
+		Encrypted bool   `yaml:"encrypted" validate:"boolean"`
+	} `yaml:"root_volume"`
 }
 
 // Bastion represents the bastion VM
@@ -102,25 +108,81 @@ type Bastion struct {
 	AllowedCIDRBlocks []string `yaml:"allowedCIDRBlocks"`
 }
 
+// Init sets default values for the DescriptorFile
+func (d DescriptorFile) Init() DescriptorFile {
+	d.FullyPrivate = false
+	d.ControlPlane.HighlyAvailable = true
+
+	// AWS
+	d.ControlPlane.AWS.AssociateOIDCProvider = true
+	d.ControlPlane.AWS.Logging.ApiServer = false
+	d.ControlPlane.AWS.Logging.Audit = false
+	d.ControlPlane.AWS.Logging.Authenticator = false
+	d.ControlPlane.AWS.Logging.ControllerManager = false
+	d.ControlPlane.AWS.Logging.Scheduler = false
+	return d
+}
+
 // Read cluster.yaml file
 func GetClusterDescriptor() (*DescriptorFile, error) {
 	descriptorRAW, err := os.ReadFile("./cluster.yaml")
 	if err != nil {
 		return nil, err
 	}
-	descriptorFile := new(DescriptorFile)
-	yaml.Unmarshal(descriptorRAW, &descriptorFile)
-	return descriptorFile, nil
+	descriptorFile := new(DescriptorFile).Init()
+	err = yaml.Unmarshal(descriptorRAW, &descriptorFile)
+	if err != nil {
+		return nil, err
+	}
+
+	validate := validator.New()
+	err = validate.Struct(descriptorFile)
+	if err != nil {
+		return nil, err
+	}
+	return &descriptorFile, nil
 }
 
 func GetClusterManifest(d DescriptorFile) (string, error) {
-	var tpl bytes.Buffer
 
-	t, err := template.ParseFS(ctel, "templates/aws.eks.tmpl")
+	var flavor string
+	switch d.InfraProvider {
+	case "aws":
+		if d.ControlPlane.Managed {
+			flavor = "templates/aws.eks.tmpl"
+		} else {
+			flavor = "templates/aws.tmpl"
+		}
+	}
+
+	funcMap := template.FuncMap{
+		"loop": func(az string) <-chan string {
+			ch := make(chan string)
+			go func() {
+				var azs []string
+				if az != "" {
+					azs = []string{az}
+
+				} else {
+					azs = []string{"a", "b", "c"}
+				}
+				for _, v := range azs {
+					ch <- v
+				}
+				close(ch)
+			}()
+			return ch
+		},
+	}
+
+	var tpl bytes.Buffer
+	t, err := template.New("").Funcs(funcMap).ParseFS(ctel, flavor)
 	if err != nil {
 		return "", err
 	}
-	err = t.Execute(&tpl, d)
+
+	fmt.Println(t.Name())
+	err = t.ExecuteTemplate(&tpl, "aws.eks.tmpl", d)
 	if err != nil {
 		return "", err
 	}
