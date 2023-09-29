4e9105357d9ed130439d31405920276c229e3141
Santiago Sanchez Paz <santiagosanchez@stratio.com>
2023-09-29 11:12:48 +0200

    Bump cluster-operator release 0.2

--- pkg/cluster/internal/create/actions/createworker/provider.go
+++ pkg/cluster/internal/create/actions/createworker/provider.go
@@ -51,8 +51,8 @@ const (
 
 	scName = "keos"
 
-	keosClusterChart = "0.1.2-6091b30"
-	keosClusterImage = "0.1.2-6091b30"
+	keosClusterChart = "0.1.2"
+	keosClusterImage = "0.1.2"
 )
 
 const machineHealthCheckWorkerNodePath = "/kind/manifests/machinehealthcheckworkernode.yaml"
