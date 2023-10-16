a82ef9cf1d0725054fafd44748db797acb67c784
lpgutierrez <92737780+lpgutierrez@users.noreply.github.com>
2023-10-16 17:57:07 +0200

    Update at.py

--- bin/at.py
+++ bin/at.py
@@ -1,11 +1,21 @@
 import os
 import json
+import yaml
 
 print("Hello world!")
 print(os.environ['groups'])
 print(os.environ['credentials'])
 
-f = open(os.environ['credentials'])
-jsonContent = json.load(f)
-print(jsonContent)
-print(jsonContent["cuenta"])
+# f = open(os.environ['credentials'])
+# jsonContent = json.load(f)
+# print(jsonContent)
+# print(jsonContent["cuenta"])
+
+filePath = os.environ['credentials']
+with open(filePath, "r") as stream:
+    try:
+        print(yaml.safe_load(stream))
+      fileContent = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+print(fileContent.aws)
