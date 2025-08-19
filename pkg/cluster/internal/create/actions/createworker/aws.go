20ce5b0273334262b630cd022157fa739b1cccab
José Alberto Novoa Rojas <112587171+iamjanr@users.noreply.github.com>
2025-08-19 11:44:47 +0200

    [PLT-2643] Bump aws-load-balancer controller version to v2.13.4 (#827)
    * Bump aws-load-balancer-controller to version v.2.13.4
    
    * Update changelog

--- pkg/cluster/internal/create/actions/createworker/aws.go
+++ pkg/cluster/internal/create/actions/createworker/aws.go
@@ -117,7 +117,7 @@ var awsCharts = ChartsDictionary{
 	Charts: map[string]map[string]map[string]commons.ChartEntry{
 		"32": {
 			"managed": {
-				"aws-load-balancer-controller": {Repository: "https://aws.github.io/eks-charts", Version: "1.11.0", Namespace: "kube-system", Pull: false, Reconcile: false},
+				"aws-load-balancer-controller": {Repository: "https://aws.github.io/eks-charts", Version: "1.13.4", Namespace: "kube-system", Pull: false, Reconcile: false},
 				"cluster-autoscaler":           {Repository: "https://kubernetes.github.io/autoscaler", Version: "9.46.6", Namespace: "kube-system", Pull: false, Reconcile: false},
 				"tigera-operator":              {Repository: "https://docs.projectcalico.org/charts", Version: "v3.30.2", Namespace: "tigera-operator", Pull: true, Reconcile: true},
 			},
